247d4f2dc9d7de74a93b3f3ab782af37b01fb9d1
Erik Kastner <kastner@gmail.com>
2008-09-03 23:33:52 -0400

    blank slate

---
+++ efm.rb
@@ -0,0 +1,15 @@
+$:.unshift "sinatra/lib"
+
+require 'sinatra'
+
+before do
+  # set UTF-8
+  header "Content-Type" => "text/html; charset=utf-8"
+
+  # set css body id
+  @body_id = "home"  
+end
+
+get "/" do
+  erb :index
+end
