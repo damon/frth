ac0d0ab125a7ed24dc3fb5ddc3ca117fd6586f9c
Bruce Williams <bruce@codefluency.com>
2008-11-03 23:50:06 -0600

    Rename fetch url to /fetch, add snaptweet parsing js

--- front-row.rb
+++ front-row.rb
@@ -32,7 +32,7 @@ get "/tweets" do
   tweets.to_json
 end
 
-get "/fetch-tweets-from-twitter" do
+get "/fetch" do
   return CACHE["tweets"] if CACHE["last_fetch"] && (Time.now - CACHE["last_fetch"]) < TWITTER_WAIT_TIMEOUT
 
   CACHE["last_fetch"] = Time.now
